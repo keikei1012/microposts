2ee62c7fcd358fed0cacb5652a5fe471e88615df
keisukeichikawa <keikei1012@yahoo.co.jp>
2016-09-23 06:47:55 +0000

    done lesson for user-pofile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  def show # 追加
+    def show # 追加
    @user = User.find(params[:id])
   end
   
@@ -17,11 +17,33 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Updated Profile!"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+  
+  before_action :correct_user, only: [:edit, :update]
+  
   private
-
+  
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location)
   end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path if @user != current_user
+  end
+  
 end
